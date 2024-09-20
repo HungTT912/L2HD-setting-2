7d75d70c2b125d94a17d5fee0f465866ddd05372
HungTT912 <noohung2003@gmail.com>
2024-09-20 16:39:45 +0700

    add tune_20

--- bash.sh
+++ bash.sh
@@ -17,4 +17,7 @@ python3 train_for_all.py --config configs/tune_20/Template-BBDM-tfbind8-l3.0-lr0
 & python3 train_for_all.py --config configs/tune_20/Template-BBDM-tfbind8-l7.0-lr0.005-d0.25.yaml --save_top \
 & python3 train_for_all.py --config configs/tune_20/Template-BBDM-tfbind8-l7.0-lr0.01-d0.25.yaml --save_top \
 & python3 train_for_all.py --config configs/tune_20/Template-BBDM-tfbind8-l7.0-lr0.05-d0.25.yaml --save_top \
-& python3 train_for_all.py --config configs/tune_20/Template-BBDM-tfbind8-l7.0-lr0.1-d0.25.yaml --save_top
+& python3 train_for_all.py --config configs/tune_20/Template-BBDM-tfbind8-l7.0-lr0.1-d0.25.yaml --save_top\
+& python3 train_for_all.py --config configs/tune_20/Template-BBDM-ant.yaml --save_top \
+& python3 train_for_all.py --config configs/tune_20/Template-BBDM-dkitty.yaml --save_top
+
