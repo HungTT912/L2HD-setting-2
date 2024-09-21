aa4df5afdf4d395939619663ae1e1cb1623927a7
cuong-dm <daomanhcuong2001@gmail.com>
2024-09-21 16:56:35 +0700

    change run_2

--- run_2.sh
+++ run_2.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #SBATCH --job-name=cuongdm        # Job name
-#SBATCH --output=log_slurm/result_cuongdm_1.txt      # Output file
-#SBATCH --error=log_slurm/error_cuongdm_1.txt        # Error file
+#SBATCH --output=log_slurm/result_cuongdm_2.txt      # Output file
+#SBATCH --error=log_slurm/error_cuongdm_2.txt        # Error file
 #SBATCH --ntasks=1               # Number of tasks (processes)
 #SBATCH --gpus=1                 # Number of GPUs per node
 #SBATCH --cpus-per-task=20                              # Number of CPU cores per task
