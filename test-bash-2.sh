09f51bdab9caa1f92ca9f41a1e4a0048c97b78c9
HungTT912 <noohung2003@gmail.com>
2024-09-21 20:03:56 +0700

    a

--- test-bash-2.sh
+++ test-bash-2.sh
@@ -1,1 +1,2 @@
-python3 test_tfbind10_tune_20-l6-lr0.05-d0.25.py --config configs/tune_20/Template-BBDM-tfbind10-l6.0-lr0.05-d0.25.yaml 
+python3 test_tfbind10_tune_20-l6-lr0.05-d0.25.py --config configs/tune_20/Template-BBDM-tfbind10-l6.0-lr0.05-d0.25.yaml \
+&python3 test_tfbind10_tune_20-l6-lr0.05-d1.0.py --config configs/tune_20/Template-BBDM-tfbind10-l6.0-lr0.05-d1.0.yaml 
