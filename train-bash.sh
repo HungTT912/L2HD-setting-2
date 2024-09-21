c9244404c2b11ab1d8d367096156f805d3ad2673
HungTT912 <noohung2003@gmail.com>
2024-09-21 07:32:01 +0700

    train tune_20

---
+++ train-bash.sh
@@ -0,0 +1,3 @@
+python3 train_for_all.py --config configs/tune_20/Template-BBDM-ant-l1.0-lr0.001-d0.25.yaml --save_top \
+& python3 train_for_all.py --config configs/tune_20/Template-BBDM-dkitty-l1.0-lr0.001-d0.25.yaml --save_top \
+& python3 train_for_all.py --config configs/tune_20/Template-BBDM-tfbind10-l6.0-lr0.05-d0.25.yaml --save_top 
