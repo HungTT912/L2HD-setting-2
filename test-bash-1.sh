aadc9b185ff796c34cecec295413712e696fba12
HungTT912 <noohung2003@gmail.com>
2024-09-23 13:49:42 +0700

    tune _20

--- test-bash-1.sh
+++ test-bash-1.sh
@@ -1,1 +1,2 @@
-python3 test_ant_tune_20-l1-lr0.001-d0.25.py --config configs/tune_20/Template-BBDM-ant-l1.0-lr0.001-d0.25.yaml 
+python3 train_for_all.py --config configs/tune_20/Template-BBDM-tfbind8-l5.5-lr0.05-d0.5.yaml \
+&python3 train_for_all.py --config configs/tune_20/Template-BBDM-tfbind10-l5.5-lr0.05-d0.5.yaml 
