a7457230f56fd1e6bf0fcb4d1e5b9bf9a4f7c77f
cuong-dm <daomanhcuong2001@gmail.com>
2024-09-24 14:39:02 +0700

    modify tune_22

--- test_tfbind8_tune_22.py
+++ test_tfbind8_tune_22.py
@@ -118,10 +118,10 @@ def main():
     for lengthscale in [5.0]:
         for delta in [0.25]: 
 
-            folder_path = './tuning_results/tune_21/result' 
+            folder_path = './tuning_results/tune_22/result' 
             if not os.path.exists(folder_path): 
                 os.makedirs(folder_path)
-            file_path = f'./tuning_results/tune_21/result/tuning_result_tfbind8_frac_{frac}_lengthscale{lengthscale}_sampling_lr{sampling_lr}_delta{delta}.csv'
+            file_path = f'./tuning_results/tune_22/result/tuning_result_tfbind8_num_fit_samples{num_fit_samples}_lengthscale{lengthscale}_sampling_lr{sampling_lr}_delta{delta}.csv'
 
             if not os.path.isfile(file_path):
                 with open(file_path, 'a') as file:
@@ -143,7 +143,7 @@ def main():
                         nconfig.model.BB.params.eta = eta 
                         for seed in seed_list:      
                             nconfig.training.classifier_free_guidance_prob = classifier_free_guidance_prob 
-                            cmd = f"grep -Rlw './results/tune_22/TFBind8-Exact-v0/num_fit_samples{num_fit_samples}//sampling_lr{sampling_lr}/initial_lengthscale{lengthscale}/delta{delta}/seed{seed}' -e 'train: true'"
+                            cmd = f"grep -Rlw './results/tune_22/TFBind8-Exact-v0/num_fit_samples{num_fit_samples}/sampling_lr{sampling_lr}/initial_lengthscale{lengthscale}/delta{delta}/seed{seed}' -e 'train: true'"
                             result_path = subprocess.check_output(cmd, shell=True, text=True)
                             result_path = result_path.strip()
                             #print(result_path)
--- train-bash.sh
+++ train-bash.sh
@@ -1,7 +1,7 @@
-python3 train_for_all.py --config configs/tune_22/Template-BBDM-tfbind8-s1000-l5.0-lr0.05-d0.25.yaml --save_top \
-&python3 train_for_all.py --config configs/tune_22/Template-BBDM-tfbind8-s2500-l5.0-lr0.05-d0.25.yaml --save_top \
+# python3 train_for_all.py --config configs/tune_22/Template-BBDM-tfbind8-s1000-l5.0-lr0.05-d0.25.yaml --save_top \
+# &python3 train_for_all.py --config configs/tune_22/Template-BBDM-tfbind8-s2500-l5.0-lr0.05-d0.25.yaml --save_top \
 
-&python3 train_for_all.py --config configs/tune_22/Template-BBDM-tfbind8-s10000-l5.0-lr0.05-d0.25.yaml --save_top \
+python3 train_for_all.py --config configs/tune_22/Template-BBDM-tfbind8-s10000-l5.0-lr0.05-d0.25.yaml --save_top \
 &python3 train_for_all.py --config configs/tune_22/Template-BBDM-tfbind8-s15000-l5.0-lr0.05-d0.25.yaml --save_top \
 &python3 test_tfbind8_tune_22.py --config configs/tune_22/Template-BBDM-tfbind8-s1000-l5.0-lr0.05-d0.25.yaml \
 &python3 test_tfbind8_tune_22.py --config configs/tune_22/Template-BBDM-tfbind8-s2500-l5.0-lr0.05-d0.25.yaml \
