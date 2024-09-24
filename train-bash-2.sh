21370f9b23dafffc68bdf0eed51854eecca01bc6
HungTT912 <noohung2003@gmail.com>
2024-09-24 19:31:52 +0700

    tune 22

--- train-bash-2.sh
+++ train-bash-2.sh
@@ -1,7 +1,3 @@
-python3 train_for_all.py --config configs/tune_22/Template-BBDM-tfbind8-s5000-l5.0-lr0.05-d0.25.yaml --save_top \
-&python3 train_for_all.py --config configs/tune_22/Template-BBDM-tfbind8-s7500-l5.0-lr0.05-d0.25.yaml --save_top 
-
-python3 test_tfbind8_tune_22.py --config configs/tune_22/Template-BBDM-tfbind8-s5000-l5.0-lr0.05-d0.25.yaml \
-&python3 train_for_all.py --config configs/tune_22/Template-BBDM-tfbind8-s20000-l5.0-lr0.05-d0.25.yaml --save_top \
-&python3 test_tfbind8_tune_22.py --config configs/tune_22/Template-BBDM-tfbind8-s7500-l5.0-lr0.05-d0.25.yaml 
-
+python3 test_tfbind8_tune_22.py --config configs/tune_22/Template-BBDM-tfbind8-s8500-l5.0-lr0.05-d0.25.yaml \
+&python3 test_tfbind8_tune_22.py --config configs/tune_22/Template-BBDM-tfbind8-s9000-l5.0-lr0.05-d0.25.yaml \
+&python3 test_tfbind8_tune_22.py --config configs/tune_22/Template-BBDM-tfbind8-s9500-l5.0-lr0.05-d0.25.yaml 
