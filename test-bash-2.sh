9087aa67141c6f0dbefef2f6cc3cb7c3c065a3f6
cuong-dm <daomanhcuong2001@gmail.com>
2024-09-25 21:54:46 +0700

    test

--- test-bash-2.sh
+++ test-bash-2.sh
@@ -1,3 +1,4 @@
 python3 test_tfbind10_tune_22_100steps_1.py --config configs/tune_22_100steps/Template-BBDM-tfbind10-s10000-l5.0-lr0.05-d0.25.yaml \
 &python3 test_tfbind10_tune_22_100steps_2.py --config configs/tune_22_100steps/Template-BBDM-tfbind10-s10000-l5.0-lr0.05-d0.25.yaml \
-&python3 test_tfbind10_tune_22_100steps_3.py --config configs/tune_22_100steps/Template-BBDM-tfbind10-s10000-l5.0-lr0.05-d0.25.yaml 
+&python3 test_tfbind10_tune_22_100steps_3.py --config configs/tune_22_100steps/Template-BBDM-tfbind10-s10000-l5.0-lr0.05-d0.25.yaml \
+&python3 test_tfbind10_tune_22_100steps_4.py --config configs/tune_22_100steps/Template-BBDM-tfbind10-s10000-l5.0-lr0.05-d0.25.yaml 
