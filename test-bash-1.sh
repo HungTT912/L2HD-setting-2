945e4991b80eb0989774b284ea640eb309929300
HungTT912 <noohung2003@gmail.com>
2024-09-25 10:36:24 +0700

    tune 22

--- test-bash-1.sh
+++ test-bash-1.sh
@@ -1,6 +1,6 @@
 python3 train_for_all.py --config configs/tune_22_100steps/Template-BBDM-tfbind8-s13000-l5.0-lr0.05-d0.25.yaml --save_top \
-&python3 train_for_all.py --config configs/tune_22_100steps/Template-BBDM-tfbind8-14000-l5.0-lr0.05-d0.25.yaml --save_top \
-&python3 train_for_all.py --config configs/tune_22_100steps/Template-BBDM-tfbind10-10000-l5.0-lr0.05-d0.25.yaml --save_top 
+&python3 train_for_all.py --config configs/tune_22_100steps/Template-BBDM-tfbind8-s14000-l5.0-lr0.05-d0.25.yaml --save_top \
+&python3 train_for_all.py --config configs/tune_22_100steps/Template-BBDM-tfbind10-s10000-l5.0-lr0.05-d0.25.yaml --save_top 
 
 
 python3 test_tfbind8_tune_22_100steps.py --config configs/tune_22_100steps/Template-BBDM-tfbind8-s13000-l5.0-lr0.05-d0.25.yaml \
--- test-bash-2.sh
+++ test-bash-2.sh
@@ -1,1 +1,1 @@
-python3 train_for_all.py --config configs/tune_22_100steps/Template-BBDM-tfbind10-10000-l5.0-lr0.05-d0.25.yaml --save_top 
+python3 train_for_all.py --config configs/tune_22_100steps/Template-BBDM-tfbind10-s10000-l5.0-lr0.05-d0.25.yaml --save_top 
