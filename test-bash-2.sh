f1903dd1a4da5585c892d99e0004af192e8dc99d
cuong-dm <daomanhcuong2001@gmail.com>
2024-09-27 09:28:59 +0700

    tune_22

--- test-bash-2.sh
+++ test-bash-2.sh
@@ -13,4 +13,4 @@ python3 train_for_all.py --config configs/ablation_studies/ab2_GP_num_of_initial
 &python3 test_tfbind10_tune_22_100steps_6.py --config configs/tune_20/Template-BBDM-tfbind10-l6.0-lr0.05-d0.25.yaml \
 &python3 test_tfbind10_tune_22_100steps_7.py --config configs/tune_20/Template-BBDM-tfbind10-l6.0-lr0.05-d0.25.yaml \
 &python3 test_tfbind10_tune_22_100steps_8.py --config configs/tune_20/Template-BBDM-tfbind10-l6.0-lr0.05-d0.25.yaml \
-&python3 test_tfbind10_tune_22_100steps_9.py --config configs/tune_20/Template-BBDM-tfbind10-l6.0-lr0.05-d0.25.yaml \
+&python3 test_tfbind10_tune_22_100steps_9.py --config configs/tune_20/Template-BBDM-tfbind10-l6.0-lr0.05-d0.25.yaml
