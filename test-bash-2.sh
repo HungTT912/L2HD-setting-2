b13c4a8011995f6d06114a0dafaab13f48c44a17
cuong-dm <daomanhcuong2001@gmail.com>
2024-09-27 11:07:39 +0700

    tune_22

--- test-bash-2.sh
+++ test-bash-2.sh
@@ -4,12 +4,4 @@ python3 test_ant_tune_22_100steps.py --config configs/tune_20/Template-BBDM-ant-
 &python3 test_tfbind10_tune_22_100steps_1.py --config configs/tune_20/Template-BBDM-tfbind10-l6.0-lr0.05-d0.25.yaml \
 &python3 test_tfbind10_tune_22_100steps_2.py --config configs/tune_20/Template-BBDM-tfbind10-l6.0-lr0.05-d0.25.yaml \
 &python3 test_tfbind10_tune_22_100steps_3.py --config configs/tune_20/Template-BBDM-tfbind10-l6.0-lr0.05-d0.25.yaml \
-&python3 train_for_all.py --config configs/ablation_studies/ab2_GP_type_of_initial_points/Template-BBDM-ant-type_points_lowest.yaml --save_top \
-&python3 train_for_all.py --config configs/ablation_studies/ab2_GP_type_of_initial_points/Template-BBDM-ant-type_points_random.yaml --save_top 
-
-python3 test_tfbind10_tune_22_100steps_4.py --config configs/tune_20/Template-BBDM-tfbind10-l6.0-lr0.05-d0.25.yaml \
-&python3 test_tfbind10_tune_22_100steps_5.py --config configs/tune_20/Template-BBDM-tfbind10-l6.0-lr0.05-d0.25.yaml \
-&python3 test_tfbind10_tune_22_100steps_6.py --config configs/tune_20/Template-BBDM-tfbind10-l6.0-lr0.05-d0.25.yaml \
-&python3 test_tfbind10_tune_22_100steps_7.py --config configs/tune_20/Template-BBDM-tfbind10-l6.0-lr0.05-d0.25.yaml \
-&python3 test_tfbind10_tune_22_100steps_8.py --config configs/tune_20/Template-BBDM-tfbind10-l6.0-lr0.05-d0.25.yaml \
-&python3 test_tfbind10_tune_22_100steps_9.py --config configs/tune_20/Template-BBDM-tfbind10-l6.0-lr0.05-d0.25.yaml
+&python3 train_for_all.py --config configs/ablation_studies/ab2_GP_type_of_initial_points/Template-BBDM-ant-type_points_lowest.yaml --save_top 
