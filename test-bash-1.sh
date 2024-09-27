995d4a66d099b33cd65e326f4eb70f81e9d16b81
cuong-dm <daomanhcuong2001@gmail.com>
2024-09-27 16:56:31 +0700

    tune_22

--- test-bash-1.sh
+++ test-bash-1.sh
@@ -1,5 +1,5 @@
 python3 train_for_all.py --config configs/ablation_studies/ab2_GP_type_of_initial_points/Template-BBDM-tfbind8-type_points_lowest.yaml --save_top \
-&python3 train_for_all.py --config configs/ablation_studies/ab2_GP_type_of_initial_points/Template-BBDM-ant-type_points_random.yaml --save_top 
+&python3 train_for_all.py --config configs/ablation_studies/ab2_GP_type_of_initial_points/Template-BBDM-tfbind8-type_points_random.yaml --save_top 
 
 python3 train_for_all.py --config configs/ablation_studies/ab3_GP_num_gradient_steps/Template-BBDM-tfbind8-grads_25.yaml --save_top \
 &python3 train_for_all.py --config configs/ablation_studies/ab3_GP_num_gradient_steps/Template-BBDM-tfbind8-grads_50.yaml --save_top 
