12ad9b270adb73085759bde5c43b52dfddbd198c
cuong-dm <daomanhcuong2001@gmail.com>
2024-09-28 00:08:06 +0700

    tune 22

--- test-bash-1.sh
+++ test-bash-1.sh
@@ -1,4 +1,1 @@
-python3 train_for_all.py --config configs/tune_22_100steps/Template-BBDM-tfbind8-full-data.yaml --save_top \
-&python3 train_for_all_1.py --config configs/ablation_studies/ab5_no_GP/Template-BBDM-tfbind8-no_GP_last_bins.yaml --save_top
-
-
+python3 train_for_all.py --config configs/tune_22_100steps/Template-BBDM-tfbind8-full-data.yaml --save_top 
