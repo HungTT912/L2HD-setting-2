c28249939bf1ba27b18d2cf258b8320058c09e74
cuong-dm <daomanhcuong2001@gmail.com>
2024-09-27 20:34:53 +0700

    run ab5

--- test-bash-1.sh
+++ test-bash-1.sh
@@ -1,4 +1,6 @@
 python3 train_for_all.py --config configs/ablation_studies/ab3_GP_num_gradient_steps/Template-BBDM-tfbind8-grads_25.yaml --save_top \
-&python3 train_for_all.py --config configs/ablation_studies/ab3_GP_num_gradient_steps/Template-BBDM-tfbind8-grads_50.yaml --save_top 
+&python3 train_for_all.py --config configs/ablation_studies/ab3_GP_num_gradient_steps/Template-BBDM-tfbind8-grads_50.yaml --save_top \
+&python3 train_for_all.py --config configs/ablation_studies/ab3_GP_num_gradient_steps/Template-BBDM-tfbind8-grads_75.yaml --save_top
+
 
 
