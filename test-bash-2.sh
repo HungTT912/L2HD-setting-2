923e92811f6ec3d9a491125e16ef64ad18d52418
cuong-dm <daomanhcuong2001@gmail.com>
2024-09-29 01:04:40 +0700

    abs

--- test-bash-2.sh
+++ test-bash-2.sh
@@ -1,1 +1,2 @@
-python3 test_tfbind10_tune_22_100steps.py --config configs/tune_22_100steps/Template-BBDM-tfbind10-s10000-l5.0-lr0.05-d0.25.yaml
+python3 test_tfbind10_tune_22_100steps.py --config configs/tune_22_100steps/Template-BBDM-tfbind10-s10000-l5.0-lr0.05-d0.25.yaml \
+&python3 test_for_num_fit_samples.py --config configs/tune_22_100steps/Template-BBDM-tfbind8-s15500-l5.0-lr0.05-d0.25.yaml
--- test_for_num_fit_samples.py
+++ test_for_num_fit_samples.py
@@ -7,12 +7,11 @@ import random
 import numpy as np
 import pandas as pd 
 import csv
+import subprocess 
+from utils import dict2namespace, get_runner, namespace2dict
 import design_bench
 import wandb 
-wandb.login(key="1cfab558732ccb32d573a7276a337d22b7d8b371")
-
-from utils import dict2namespace, get_runner, namespace2dict
-
+import time 
 
 def parse_args_and_config():
     parser = argparse.ArgumentParser(description=globals()['__doc__'])
@@ -64,6 +63,33 @@ def set_random_seed(SEED=1234):
     torch.backends.cudnn.benchmark = False
     torch.backends.cudnn.deterministic = True
 
+def get_offline_data(nconfig):
+    if nconfig.task.name != 'TFBind10-Exact-v0':
+        task = design_bench.make(nconfig.task.name)
+    else:
+        task = design_bench.make(nconfig.task.name,
+                                dataset_kwargs={"max_samples": 10000})
+
+    offline_x = task.x
+    if task.is_discrete:
+        offline_x = task.to_logits(offline_x).reshape(offline_x.shape[0], -1)
+
+    mean_x = np.mean(offline_x, axis=0)
+    std_x = np.std(offline_x, axis=0)
+    std_x = np.where(std_x == 0, 1.0, std_x)
+    
+    offline_y = task.y
+    mean_y = np.mean(offline_y, axis=0)
+    std_y = np.std(offline_y, axis=0)
+    
+    # shuffle_idx = np.random.permutation(offline_x.shape[0])
+
+    # offline_x = offline_x[shuffle_idx]
+    # offline_y = offline_y[shuffle_idx]
+    offline_y = offline_y.reshape(-1)
+    
+    return torch.from_numpy(offline_x), torch.from_numpy(mean_x), torch.from_numpy(std_x), torch.from_numpy(offline_y), torch.from_numpy(mean_y), torch.from_numpy(std_y)
+
 
 def CPU_singleGPU_launcher(config):
     set_random_seed(config.args.seed)
@@ -75,14 +101,27 @@ def CPU_singleGPU_launcher(config):
             runner.test()
     return
 
+
 def trainer(config): 
     set_random_seed(config.args.seed)
     runner = get_runner(config.runner, config)
     return runner.train()
-def tester(config,task):
+def tester(config, task):
+    global offline_x_list, mean_x_list, std_x_list, offline_y_list, mean_y_list, std_y_list 
+    offline_x = offline_x_list[config.args.seed] 
+    offline_y = offline_y_list[config.args.seed]
+    mean_x = mean_x_list[config.args.seed] 
+    mean_y = mean_y_list[config.args.seed] 
+    std_x = std_x_list[config.args.seed] 
+    std_y = std_y_list[config.args.seed] 
+    
     set_random_seed(config.args.seed)
     runner = get_runner(config.runner, config)
+    runner.offline_x, runner.mean_offline_x, runner.std_offline_x = offline_x, mean_x, std_x 
+    runner.offline_y, runner.mean_offline_y, runner.std_offline_y = offline_y, mean_y, std_y 
+    
     return runner.test(task) 
+
 
 def main():
     nconfig, dconfig = parse_args_and_config()
@@ -119,7 +158,31 @@ def main():
         'TFBind8-Exact-v0': 'results/tune_22_100steps/TFBind8-Exact-v0/num_fit_samples15500/sampling_lr0.05/initial_lengthscale5.0/delta0.25',
         'TFBind10-Exact-v0' : 'results/tune_22_100steps/TFBind10-Exact-v0/num_fit_samples10000/sampling_lr0.05/initial_lengthscale5.0/delta0.25'
     }
-    hyper = [[0.5,0.85,-4.0], [0.5,0.90,-3.5],[0.5,1.00,-3.0], [0.5,0.80,-4.0]]
+    global offline_x_list, mean_x_list, std_x_list, offline_y_list, mean_y_list, std_y_list 
+    offline_x_list, mean_x_list, std_x_list, offline_y_list, mean_y_list, std_y_list = [],[],[],[],[],[] 
+    for seed in seed_list : 
+        global offline_x, mean_x, std_x, offline_y, mean_y, std_y 
+        set_random_seed(seed)
+        offline_x, mean_x, std_x , offline_y, mean_y , std_y = get_offline_data(nconfig)
+        offline_x = (offline_x - mean_x) / std_x
+        offline_y = (offline_y - mean_y) / std_y   
+        shuffle_idx = np.random.permutation(offline_x.shape[0])
+        offline_x = offline_x[shuffle_idx]
+        offline_y = offline_y[shuffle_idx]
+        offline_x = offline_x.to(nconfig.training.device[0])
+        offline_y = offline_y.to(nconfig.training.device[0])
+        sorted_indices = torch.argsort(offline_y)[-128:] 
+        offline_x = offline_x[sorted_indices] 
+        offline_y = offline_y[sorted_indices] 
+        
+        offline_x_list.append(offline_x) 
+        offline_y_list.append(offline_y) 
+        mean_x_list.append(mean_x) 
+        std_x_list.append(std_x) 
+        mean_y_list.append(mean_y)
+        std_y_list.append(std_y) 
+        
+    hyper = [[0.5,0.80,-4.0], [0.5,0.85,-4.0], [0.5,0.90,-3.5],[0.5,1.00,-3.0]]
     for eta, alpha, w in hyper : 
         for num_fit_samples in [15500, 15000, 14500, 16000, 17000, 9000, 10000, 13000, 14000, 14500, 7500,8000,8500] :
             folder_path = f'results/tune_22_100steps/TFBind8-Exact-v0/num_fit_samples{num_fit_samples}/sampling_lr0.05/initial_lengthscale5.0/delta0.25'
