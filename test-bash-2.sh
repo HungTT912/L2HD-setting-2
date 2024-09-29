d395bfaa0997f2dcd311856442cb3cac60bd2d3e
cuong-dm <daomanhcuong2001@gmail.com>
2024-09-29 13:08:24 +0700

    abs

--- test-bash-2.sh
+++ test-bash-2.sh
@@ -1,1 +1,1 @@
-python3 test_tfbind10_tune_20_100steps_l6.py --config configs/tune_20/Template-BBDM-tfbind10-l6.0-lr0.05-d0.25.yaml
+python3 test_tfbind10_tune_22_100steps.py --config configs/tune_22_100steps/Template-BBDM-tfbind10-s10000-l5.0-lr0.05-d0.25.yaml 
