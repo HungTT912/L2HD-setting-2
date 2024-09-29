d83ffc936aba6ea5800af42163c3014da2abec6e
cuong-dm <daomanhcuong2001@gmail.com>
2024-09-29 17:17:41 +0700

    abs

--- test-bash-1.sh
+++ test-bash-1.sh
@@ -1,5 +1,5 @@
-python3 test_tfbind10_tune_22_100_steps_6.py --config configs/tune_22_100steps/Template-BBDM-tfbind10-s10000-l5.0-lr0.05-d0.25.yaml \
-&python3 test_tfbind10_tune_22_100_steps_7.py --config configs/tune_22_100steps/Template-BBDM-tfbind10-s10000-l5.0-lr0.05-d0.25.yaml \
-&python3 test_tfbind10_tune_22_100_steps_8.py --config configs/tune_22_100steps/Template-BBDM-tfbind10-s10000-l5.0-lr0.05-d0.25.yaml \
-&python3 test_tfbind10_tune_22_100_steps_9.py --config configs/tune_22_100steps/Template-BBDM-tfbind10-s10000-l5.0-lr0.05-d0.25.yaml \
-&python3 test_tfbind10_tune_22_100_steps_10.py --config configs/tune_22_100steps/Template-BBDM-tfbind10-s10000-l5.0-lr0.05-d0.25.yaml 
+python3 test_tfbind10_tune_22_100steps_6.py --config configs/tune_22_100steps/Template-BBDM-tfbind10-s10000-l5.0-lr0.05-d0.25.yaml \
+&python3 test_tfbind10_tune_22_100steps_7.py --config configs/tune_22_100steps/Template-BBDM-tfbind10-s10000-l5.0-lr0.05-d0.25.yaml \
+&python3 test_tfbind10_tune_22_100steps_8.py --config configs/tune_22_100steps/Template-BBDM-tfbind10-s10000-l5.0-lr0.05-d0.25.yaml \
+&python3 test_tfbind10_tune_22_100steps_9.py --config configs/tune_22_100steps/Template-BBDM-tfbind10-s10000-l5.0-lr0.05-d0.25.yaml \
+&python3 test_tfbind10_tune_22_100steps_10.py --config configs/tune_22_100steps/Template-BBDM-tfbind10-s10000-l5.0-lr0.05-d0.25.yaml 
