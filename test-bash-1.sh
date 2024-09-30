f3a2c3656e713d5b18fdb8cec7520fd8ae53b4bd
cuong-dm <daomanhcuong2001@gmail.com>
2024-09-30 09:18:00 +0700

    .

--- test-bash-1.sh
+++ test-bash-1.sh
@@ -2,4 +2,3 @@ python3 train_for_all_1.py --config configs/tune_22_100steps/Template-BBDM-tfbin
 &python3 train_for_all_1.py --config configs/tune_22_100steps/Template-BBDM-tfbind10-s10000-l5.0-lr0.05-d0.25.yaml --save_top \
 &python3 train_for_all_1.py --config configs/tune_20/Template-BBDM-dkitty-l1.0-lr0.001-d0.25.yaml --save_top \
 &python3 train_for_all_1.py --config configs/tune_20/Template-BBDM-ant-l1.0-lr0.001-d0.25.yamll --save_top 
-&
