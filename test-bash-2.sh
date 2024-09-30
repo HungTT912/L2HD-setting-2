89874a4e9f66081daff525fd23e4a04f3fa22139
HungTT912 <noohung2003@gmail.com>
2024-09-30 17:03:40 +0700

    abs

--- test-bash-2.sh
+++ test-bash-2.sh
@@ -1,7 +1,6 @@
 python3 train_and_test_for_all.py --config configs/ablation_studies/ab5_no_GP/Template-BBDM-tfbind8-GP-delta0.yaml --save_top \
 &python3 train_and_test_for_all.py --config configs/ablation_studies/ab2_GP_type_of_initial_points/Template-BBDM-tfbind8-type_points_lowest.yaml --save_top \
 &python3 test_for_ab3.py --config configs/ablation_studies/ab3_GP_num_gradient_steps/Template-BBDM-ant-grads_25.yaml \
-&python3 test_for_ab4.py --config configs/ablation_studies/ab4_type_of_conditioning_points/Template-BBDM-ant-highest.yaml \
 &python3 test_for_ab4.py --config configs/ablation_studies/ab4_type_of_conditioning_points/Template-BBDM-tfbind8-highest.yaml 
 
 
