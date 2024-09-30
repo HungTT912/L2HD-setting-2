0cab85f5bc3376c1db2eab858f3c5b8509675b7d
HungTT912 <noohung2003@gmail.com>
2024-09-30 17:54:11 +0700

    a

--- test-bash-2.sh
+++ test-bash-2.sh
@@ -1,8 +1,7 @@
-python3 train_and_test_for_all.py --config configs/ablation_studies/ab5_no_GP/Template-BBDM-tfbind8-GP-delta0.yaml --save_top \
-&python3 train_and_test_for_all.py --config configs/ablation_studies/ab2_GP_type_of_initial_points/Template-BBDM-tfbind8-type_points_lowest.yaml --save_top \
-&python3 test_for_ab3.py --config configs/ablation_studies/ab3_GP_num_gradient_steps/Template-BBDM-ant-grads_25.yaml \
-&python3 test_for_ab4.py --config configs/ablation_studies/ab4_type_of_conditioning_points/Template-BBDM-tfbind8-highest.yaml 
-
+python3 train_and_test_for_all.py --config configs/ablation_studies/ab2_GP_type_of_initial_points/Template-BBDM-tfbind8-type_points_lowest.yaml --save_top \
+&python3 test_for_ab4.py --config configs/ablation_studies/ab4_type_of_conditioning_points/Template-BBDM-tfbind8-highest.yaml \
+&python3 test_for_ab3.py --config configs/ablation_studies/ab3_GP_num_gradient_steps/Template-BBDM-ant-grads_25.yaml
 
 python3 train_and_test_for_all.py --config configs/ablation_studies/ab2_GP_type_of_initial_points/Template-BBDM-tfbind8-type_points_random.yaml --save_top \
-&python3 test_for_ab5.py --config configs/ablation_studies/ab5_no_GP/Template-BBDM-ant-GP-delta0.yaml 
+&python3 test_for_ab5.py --config configs/ablation_studies/ab5_no_GP/Template-BBDM-ant-GP-delta0.yaml \
+&python3 train_and_test_for_all.py --config configs/ablation_studies/ab5_no_GP/Template-BBDM-tfbind8-GP-delta0.yaml --save_top 
