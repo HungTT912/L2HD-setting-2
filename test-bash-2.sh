ecf3b10e600c79126e0b9d794e553b21611f6a1e
HungTT912 <noohung2003@gmail.com>
2024-09-30 22:16:48 +0700

    a

--- test-bash-2.sh
+++ test-bash-2.sh
@@ -1,6 +1,5 @@
 python3 test_for_ab5.py --config configs/ablation_studies/ab5_no_GP/Template-BBDM-ant-GP-delta0.yaml \
-&python3 test_for_ab5.py --config configs/ablation_studies/ab5_no_GP/Template-BBDM-tfbind8-GP-delta0.yaml \
-&python3 train_and_test_for_all.py --config configs/ablation_studies/ab2_GP_type_of_initial_points/Template-BBDM-ant-type_points_random.yaml 
+&python3 test_for_ab5.py --config configs/ablation_studies/ab5_no_GP/Template-BBDM-tfbind8-GP-delta0.yaml 
 
 python3 test_for_ab4.py --config configs/ablation_studies/ab4_type_of_conditioning_points/Template-BBDM-ant-highest.yaml \
-&python3 test_for_ab4.py --config configs/ablation_studies/ab4_type_of_conditioning_points/Template-BBDM-tfbind8-highest.yaml 
+&python3 train_and_test_for_all.py --config configs/ablation_studies/ab2_GP_type_of_initial_points/Template-BBDM-tfbind8-type_points_random.yaml 
