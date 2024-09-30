959b8db6c714acb2db6d4e80c6379fdc99215675
HungTT912 <noohung2003@gmail.com>
2024-09-30 09:02:43 +0700

    a

--- test-bash-1.sh
+++ test-bash-1.sh
@@ -1,1 +1,5 @@
-python3 test_tfbind8_tune_22_100steps.py --config configs/tune_22_100steps/Template-BBDM-tfbind8-s14000-l5.0-lr0.05-d0.25.yaml 
+python3 train_for_all_1.py --config configs/tune_22_100steps/Template-BBDM-tfbind8-s17000-l5.0-lr0.05-d0.25.yaml --save_top \
+&python3 train_for_all_1.py --config configs/tune_22_100steps/Template-BBDM-tfbind10-s10000-l5.0-lr0.05-d0.25.yaml --save_top \
+&python3 train_for_all_1.py --config configs/tune_20/Template-BBDM-dkitty-l1.0-lr0.001-d0.25.yaml --save_top \
+&python3 train_for_all_1.py --config configs/tune_20/Template-BBDM-ant-l1.0-lr0.001-d0.25.yamll --save_top 
+&
