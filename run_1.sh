4bfa088e3769eff7bda1a6988cee2d4a4b2e0535
HungTT912 <noohung2003@gmail.com>
2024-10-09 11:03:47 +0700

    add tune23

--- run_1.sh
+++ run_1.sh
@@ -5,4 +5,4 @@
 #SBATCH --ntasks=1               # Number of tasks (processes)
 #SBATCH --gpus=1                 # Number of GPUs per node
 #SBATCH --cpus-per-task=20                              # Number of CPU cores per task
-sh ./test-bash-1.sh
+sh ./train-bash.sh
