e4f8eaba055a72397cd685b8cf1a545994314b20
HungTT912 <noohung2003@gmail.com>
2024-10-19 14:21:51 +0700

    tune 23

--- train-bash.sh
+++ train-bash.sh
@@ -25,8 +25,8 @@
 
 # python3 train_for_all_1.py --config configs/tune_23/Template-BBDM-tfbind10-s10000-l5.75-lr0.05-d0.25.yaml --save_top 
 
-python3 test_tfbind10_tune_23.py --config configs/tune_23/Template-BBDM-tfbind10-s10000-l5.75-lr0.05-d0.25.yaml \
-&python3 test_tfbind10_tune_23_1.py --config configs/tune_23/Template-BBDM-tfbind10-s10000-l5.75-lr0.05-d0.25.yaml 
+# python3 test_tfbind10_tune_23.py --config configs/tune_23/Template-BBDM-tfbind10-s10000-l5.75-lr0.05-d0.25.yaml \
+# &python3 test_tfbind10_tune_23_1.py --config configs/tune_23/Template-BBDM-tfbind10-s10000-l5.75-lr0.05-d0.25.yaml 
 
 # python3 train_for_all_1.py --config configs/tune_23/Template-BBDM-tfbind8-s10000-l6.25-lr0.05-d0.25.yaml --save_top \
 # &python3 train_for_all_1.py --config configs/tune_23/Template-BBDM-tfbind8-s18000-l6.25-lr0.05-d0.25.yaml --save_top 
@@ -54,8 +54,8 @@ python3 test_tfbind10_tune_23.py --config configs/tune_23/Template-BBDM-tfbind10
 
 # python3 train_for_all_1.py --config configs/tune_23/Template-BBDM-tfbind10-s10000-l6.25-lr0.05-d0.25.yaml --save_top 
 
-python3 test_tfbind10_tune_23.py --config configs/tune_23/Template-BBDM-tfbind10-s10000-l6.25-lr0.05-d0.25.yaml \
-&python3 test_tfbind10_tune_23_1.py --config configs/tune_23/Template-BBDM-tfbind10-s10000-l6.25-lr0.05-d0.25.yaml 
+# python3 test_tfbind10_tune_23.py --config configs/tune_23/Template-BBDM-tfbind10-s10000-l6.25-lr0.05-d0.25.yaml \
+# &python3 test_tfbind10_tune_23_1.py --config configs/tune_23/Template-BBDM-tfbind10-s10000-l6.25-lr0.05-d0.25.yaml 
 
 
 # python3 train_for_all_1.py --config configs/tune_23/Template-BBDM-tfbind8-s10000-l6.5-lr0.05-d0.25.yaml --save_top \
@@ -85,63 +85,63 @@ python3 test_tfbind10_tune_23.py --config configs/tune_23/Template-BBDM-tfbind10
 # python3 train_for_all_1.py --config configs/tune_23/Template-BBDM-tfbind10-s10000-l6.5-lr0.05-d0.25.yaml --save_top 
 
 
-python3 test_tfbind10_tune_23.py --config configs/tune_23/Template-BBDM-tfbind10-s10000-l6.5-lr0.05-d0.25.yaml \
-&python3 test_tfbind10_tune_23_1.py --config configs/tune_23/Template-BBDM-tfbind10-s10000-l6.5-lr0.05-d0.25.yaml 
+# python3 test_tfbind10_tune_23.py --config configs/tune_23/Template-BBDM-tfbind10-s10000-l6.5-lr0.05-d0.25.yaml \
+# &python3 test_tfbind10_tune_23_1.py --config configs/tune_23/Template-BBDM-tfbind10-s10000-l6.5-lr0.05-d0.25.yaml 
 
 #----5.25
-python3 train_for_all_1.py --config configs/tune_23/Template-BBDM-tfbind8-s10000-l5.25-lr0.05-d0.25.yaml --save_top \
-&python3 train_for_all_1.py --config configs/tune_23/Template-BBDM-tfbind8-s18000-l5.25-lr0.05-d0.25.yaml --save_top 
+# python3 train_for_all_1.py --config configs/tune_23/Template-BBDM-tfbind8-s10000-l5.25-lr0.05-d0.25.yaml --save_top \
+# &python3 train_for_all_1.py --config configs/tune_23/Template-BBDM-tfbind8-s18000-l5.25-lr0.05-d0.25.yaml --save_top 
 
-python3 train_for_all_1.py --config configs/tune_23/Template-BBDM-tfbind8-s12000-l5.25-lr0.05-d0.25.yaml --save_top \
-&python3 train_for_all_1.py --config configs/tune_23/Template-BBDM-tfbind8-s13000-l5.25-lr0.05-d0.25.yaml --save_top 
+# python3 train_for_all_1.py --config configs/tune_23/Template-BBDM-tfbind8-s12000-l5.25-lr0.05-d0.25.yaml --save_top \
+# &python3 train_for_all_1.py --config configs/tune_23/Template-BBDM-tfbind8-s13000-l5.25-lr0.05-d0.25.yaml --save_top 
 
-python3 train_for_all_1.py --config configs/tune_23/Template-BBDM-tfbind8-s14000-l5.25-lr0.05-d0.25.yaml --save_top \
-&python3 train_for_all_1.py --config configs/tune_23/Template-BBDM-tfbind8-s15000-l5.25-lr0.05-d0.25.yaml --save_top 
+# python3 train_for_all_1.py --config configs/tune_23/Template-BBDM-tfbind8-s14000-l5.25-lr0.05-d0.25.yaml --save_top \
+# &python3 train_for_all_1.py --config configs/tune_23/Template-BBDM-tfbind8-s15000-l5.25-lr0.05-d0.25.yaml --save_top 
 
-python3 train_for_all_1.py --config configs/tune_23/Template-BBDM-tfbind8-s16000-l5.25-lr0.05-d0.25.yaml --save_top \
-&python3 train_for_all_1.py --config configs/tune_23/Template-BBDM-tfbind8-s17000-l5.25-lr0.05-d0.25.yaml --save_top 
-
-
-python3 test_tfbind8_tune_23.py --config configs/tune_23/Template-BBDM-tfbind8-s12000-l5.25-lr0.05-d0.25.yaml \
-&python3 test_tfbind8_tune_23.py --config configs/tune_23/Template-BBDM-tfbind8-s13000-l5.25-lr0.05-d0.25.yaml \
-&python3 test_tfbind8_tune_23.py --config configs/tune_23/Template-BBDM-tfbind8-s14000-l5.25-lr0.05-d0.25.yaml \
-&python3 test_tfbind8_tune_23.py --config configs/tune_23/Template-BBDM-tfbind8-s16000-l5.25-lr0.05-d0.25.yaml  
+# python3 train_for_all_1.py --config configs/tune_23/Template-BBDM-tfbind8-s16000-l5.25-lr0.05-d0.25.yaml --save_top \
+# &python3 train_for_all_1.py --config configs/tune_23/Template-BBDM-tfbind8-s17000-l5.25-lr0.05-d0.25.yaml --save_top 
 
 
-python3 test_tfbind8_tune_23.py --config configs/tune_23/Template-BBDM-tfbind8-s10000-l5.25-lr0.05-d0.25.yaml \
-&python3 test_tfbind8_tune_23.py --config configs/tune_23/Template-BBDM-tfbind8-s15000-l5.25-lr0.05-d0.25.yaml \
-&python3 test_tfbind8_tune_23.py --config configs/tune_23/Template-BBDM-tfbind8-s17000-l5.25-lr0.05-d0.25.yaml \
-&python3 test_tfbind8_tune_23.py --config configs/tune_23/Template-BBDM-tfbind8-s18000-l5.25-lr0.05-d0.25.yaml 
+# python3 test_tfbind8_tune_23.py --config configs/tune_23/Template-BBDM-tfbind8-s12000-l5.25-lr0.05-d0.25.yaml \
+# &python3 test_tfbind8_tune_23.py --config configs/tune_23/Template-BBDM-tfbind8-s13000-l5.25-lr0.05-d0.25.yaml \
+# &python3 test_tfbind8_tune_23.py --config configs/tune_23/Template-BBDM-tfbind8-s14000-l5.25-lr0.05-d0.25.yaml \
+# &python3 test_tfbind8_tune_23.py --config configs/tune_23/Template-BBDM-tfbind8-s16000-l5.25-lr0.05-d0.25.yaml  
 
-python3 train_for_all_1.py --config configs/tune_23/Template-BBDM-tfbind10-s10000-l5.25-lr0.05-d0.25.yaml --save_top 
 
-python3 test_tfbind10_tune_23.py --config configs/tune_23/Template-BBDM-tfbind10-l5.25-lr0.05-d0.25.yaml \
-&python3 test_tfbind10_tune_23_1.py --config configs/tune_23/Template-BBDM-tfbind10-l5.25-lr0.05-d0.25.yaml 
+# python3 test_tfbind8_tune_23.py --config configs/tune_23/Template-BBDM-tfbind8-s10000-l5.25-lr0.05-d0.25.yaml \
+# &python3 test_tfbind8_tune_23.py --config configs/tune_23/Template-BBDM-tfbind8-s15000-l5.25-lr0.05-d0.25.yaml \
+# &python3 test_tfbind8_tune_23.py --config configs/tune_23/Template-BBDM-tfbind8-s17000-l5.25-lr0.05-d0.25.yaml \
+# &python3 test_tfbind8_tune_23.py --config configs/tune_23/Template-BBDM-tfbind8-s18000-l5.25-lr0.05-d0.25.yaml 
+
+# python3 train_for_all_1.py --config configs/tune_23/Template-BBDM-tfbind10-s10000-l5.25-lr0.05-d0.25.yaml --save_top 
+
+# python3 test_tfbind10_tune_23.py --config configs/tune_23/Template-BBDM-tfbind10-l5.25-lr0.05-d0.25.yaml \
+# &python3 test_tfbind10_tune_23_1.py --config configs/tune_23/Template-BBDM-tfbind10-l5.25-lr0.05-d0.25.yaml 
 
 #----5.7
-python3 train_for_all_1.py --config configs/tune_23/Template-BBDM-tfbind8-s10000-l5.7-lr0.05-d0.25.yaml --save_top \
-&python3 train_for_all_1.py --config configs/tune_23/Template-BBDM-tfbind8-s18000-l5.7-lr0.05-d0.25.yaml --save_top 
+# python3 train_for_all_1.py --config configs/tune_23/Template-BBDM-tfbind8-s10000-l5.7-lr0.05-d0.25.yaml --save_top \
+# &python3 train_for_all_1.py --config configs/tune_23/Template-BBDM-tfbind8-s18000-l5.7-lr0.05-d0.25.yaml --save_top 
 
-python3 train_for_all_1.py --config configs/tune_23/Template-BBDM-tfbind8-s12000-l5.7-lr0.05-d0.25.yaml --save_top \
-&python3 train_for_all_1.py --config configs/tune_23/Template-BBDM-tfbind8-s13000-l5.7-lr0.05-d0.25.yaml --save_top 
+# python3 train_for_all_1.py --config configs/tune_23/Template-BBDM-tfbind8-s12000-l5.7-lr0.05-d0.25.yaml --save_top \
+# &python3 train_for_all_1.py --config configs/tune_23/Template-BBDM-tfbind8-s13000-l5.7-lr0.05-d0.25.yaml --save_top 
 
-python3 train_for_all_1.py --config configs/tune_23/Template-BBDM-tfbind8-s14000-l5.7-lr0.05-d0.25.yaml --save_top \
-&python3 train_for_all_1.py --config configs/tune_23/Template-BBDM-tfbind8-s15000-l5.7-lr0.05-d0.25.yaml --save_top 
+# python3 train_for_all_1.py --config configs/tune_23/Template-BBDM-tfbind8-s14000-l5.7-lr0.05-d0.25.yaml --save_top \
+# &python3 train_for_all_1.py --config configs/tune_23/Template-BBDM-tfbind8-s15000-l5.7-lr0.05-d0.25.yaml --save_top 
 
-python3 train_for_all_1.py --config configs/tune_23/Template-BBDM-tfbind8-s16000-l5.7-lr0.05-d0.25.yaml --save_top \
-&python3 train_for_all_1.py --config configs/tune_23/Template-BBDM-tfbind8-s17000-l5.7-lr0.05-d0.25.yaml --save_top 
-
-
-python3 test_tfbind8_tune_23.py --config configs/tune_23/Template-BBDM-tfbind8-s12000-l5.7-lr0.05-d0.25.yaml \
-&python3 test_tfbind8_tune_23.py --config configs/tune_23/Template-BBDM-tfbind8-s13000-l5.7-lr0.05-d0.25.yaml \
-&python3 test_tfbind8_tune_23.py --config configs/tune_23/Template-BBDM-tfbind8-s14000-l5.7-lr0.05-d0.25.yaml \
-&python3 test_tfbind8_tune_23.py --config configs/tune_23/Template-BBDM-tfbind8-s16000-l5.7-lr0.05-d0.25.yaml  
+# python3 train_for_all_1.py --config configs/tune_23/Template-BBDM-tfbind8-s16000-l5.7-lr0.05-d0.25.yaml --save_top \
+# &python3 train_for_all_1.py --config configs/tune_23/Template-BBDM-tfbind8-s17000-l5.7-lr0.05-d0.25.yaml --save_top 
 
 
-python3 test_tfbind8_tune_23.py --config configs/tune_23/Template-BBDM-tfbind8-s10000-l5.7-lr0.05-d0.25.yaml \
-&python3 test_tfbind8_tune_23.py --config configs/tune_23/Template-BBDM-tfbind8-s15000-l5.7-lr0.05-d0.25.yaml \
-&python3 test_tfbind8_tune_23.py --config configs/tune_23/Template-BBDM-tfbind8-s17000-l5.7-lr0.05-d0.25.yaml \
-&python3 test_tfbind8_tune_23.py --config configs/tune_23/Template-BBDM-tfbind8-s18000-l5.7-lr0.05-d0.25.yaml 
+# python3 test_tfbind8_tune_23.py --config configs/tune_23/Template-BBDM-tfbind8-s12000-l5.7-lr0.05-d0.25.yaml \
+# &python3 test_tfbind8_tune_23.py --config configs/tune_23/Template-BBDM-tfbind8-s13000-l5.7-lr0.05-d0.25.yaml \
+# &python3 test_tfbind8_tune_23.py --config configs/tune_23/Template-BBDM-tfbind8-s14000-l5.7-lr0.05-d0.25.yaml \
+# &python3 test_tfbind8_tune_23.py --config configs/tune_23/Template-BBDM-tfbind8-s16000-l5.7-lr0.05-d0.25.yaml  
+
+
+# python3 test_tfbind8_tune_23.py --config configs/tune_23/Template-BBDM-tfbind8-s10000-l5.7-lr0.05-d0.25.yaml \
+# &python3 test_tfbind8_tune_23.py --config configs/tune_23/Template-BBDM-tfbind8-s15000-l5.7-lr0.05-d0.25.yaml \
+# &python3 test_tfbind8_tune_23.py --config configs/tune_23/Template-BBDM-tfbind8-s17000-l5.7-lr0.05-d0.25.yaml \
+# &python3 test_tfbind8_tune_23.py --config configs/tune_23/Template-BBDM-tfbind8-s18000-l5.7-lr0.05-d0.25.yaml 
 
 python3 train_for_all_1.py --config configs/tune_23/Template-BBDM-tfbind10-s10000-l5.7-lr0.05-d0.25.yaml --save_top 
 
@@ -149,31 +149,31 @@ python3 test_tfbind10_tune_23.py --config configs/tune_23/Template-BBDM-tfbind10
 &python3 test_tfbind10_tune_23_1.py --config configs/tune_23/Template-BBDM-tfbind10-l5.7-lr0.05-d0.25.yaml 
 
 #----5.8
-python3 train_for_all_1.py --config configs/tune_23/Template-BBDM-tfbind8-s10000-l5.8-lr0.05-d0.25.yaml --save_top \
-&python3 train_for_all_1.py --config configs/tune_23/Template-BBDM-tfbind8-s18000-l5.8-lr0.05-d0.25.yaml --save_top 
+# python3 train_for_all_1.py --config configs/tune_23/Template-BBDM-tfbind8-s10000-l5.8-lr0.05-d0.25.yaml --save_top \
+# &python3 train_for_all_1.py --config configs/tune_23/Template-BBDM-tfbind8-s18000-l5.8-lr0.05-d0.25.yaml --save_top 
 
-python3 train_for_all_1.py --config configs/tune_23/Template-BBDM-tfbind8-s12000-l5.8-lr0.05-d0.25.yaml --save_top \
-&python3 train_for_all_1.py --config configs/tune_23/Template-BBDM-tfbind8-s13000-l5.8-lr0.05-d0.25.yaml --save_top 
+# python3 train_for_all_1.py --config configs/tune_23/Template-BBDM-tfbind8-s12000-l5.8-lr0.05-d0.25.yaml --save_top \
+# &python3 train_for_all_1.py --config configs/tune_23/Template-BBDM-tfbind8-s13000-l5.8-lr0.05-d0.25.yaml --save_top 
 
-python3 train_for_all_1.py --config configs/tune_23/Template-BBDM-tfbind8-s14000-l5.8-lr0.05-d0.25.yaml --save_top \
-&python3 train_for_all_1.py --config configs/tune_23/Template-BBDM-tfbind8-s15000-l5.8-lr0.05-d0.25.yaml --save_top 
+# python3 train_for_all_1.py --config configs/tune_23/Template-BBDM-tfbind8-s14000-l5.8-lr0.05-d0.25.yaml --save_top \
+# &python3 train_for_all_1.py --config configs/tune_23/Template-BBDM-tfbind8-s15000-l5.8-lr0.05-d0.25.yaml --save_top 
 
-python3 train_for_all_1.py --config configs/tune_23/Template-BBDM-tfbind8-s16000-l5.8-lr0.05-d0.25.yaml --save_top \
-&python3 train_for_all_1.py --config configs/tune_23/Template-BBDM-tfbind8-s17000-l5.8-lr0.05-d0.25.yaml --save_top 
-
-
-python3 test_tfbind8_tune_23.py --config configs/tune_23/Template-BBDM-tfbind8-s12000-l5.8-lr0.05-d0.25.yaml \
-&python3 test_tfbind8_tune_23.py --config configs/tune_23/Template-BBDM-tfbind8-s13000-l5.8-lr0.05-d0.25.yaml \
-&python3 test_tfbind8_tune_23.py --config configs/tune_23/Template-BBDM-tfbind8-s14000-l5.8-lr0.05-d0.25.yaml \
-&python3 test_tfbind8_tune_23.py --config configs/tune_23/Template-BBDM-tfbind8-s16000-l5.8-lr0.05-d0.25.yaml  
+# python3 train_for_all_1.py --config configs/tune_23/Template-BBDM-tfbind8-s16000-l5.8-lr0.05-d0.25.yaml --save_top \
+# &python3 train_for_all_1.py --config configs/tune_23/Template-BBDM-tfbind8-s17000-l5.8-lr0.05-d0.25.yaml --save_top 
 
 
-python3 test_tfbind8_tune_23.py --config configs/tune_23/Template-BBDM-tfbind8-s10000-l5.8-lr0.05-d0.25.yaml \
-&python3 test_tfbind8_tune_23.py --config configs/tune_23/Template-BBDM-tfbind8-s15000-l5.8-lr0.05-d0.25.yaml \
-&python3 test_tfbind8_tune_23.py --config configs/tune_23/Template-BBDM-tfbind8-s17000-l5.8-lr0.05-d0.25.yaml \
-&python3 test_tfbind8_tune_23.py --config configs/tune_23/Template-BBDM-tfbind8-s18000-l5.8-lr0.05-d0.25.yaml 
+# python3 test_tfbind8_tune_23.py --config configs/tune_23/Template-BBDM-tfbind8-s12000-l5.8-lr0.05-d0.25.yaml \
+# &python3 test_tfbind8_tune_23.py --config configs/tune_23/Template-BBDM-tfbind8-s13000-l5.8-lr0.05-d0.25.yaml \
+# &python3 test_tfbind8_tune_23.py --config configs/tune_23/Template-BBDM-tfbind8-s14000-l5.8-lr0.05-d0.25.yaml \
+# &python3 test_tfbind8_tune_23.py --config configs/tune_23/Template-BBDM-tfbind8-s16000-l5.8-lr0.05-d0.25.yaml  
 
-python3 train_for_all_1.py --config configs/tune_23/Template-BBDM-tfbind10-s10000-l5.8-lr0.05-d0.25.yaml --save_top 
+
+# python3 test_tfbind8_tune_23.py --config configs/tune_23/Template-BBDM-tfbind8-s10000-l5.8-lr0.05-d0.25.yaml \
+# &python3 test_tfbind8_tune_23.py --config configs/tune_23/Template-BBDM-tfbind8-s15000-l5.8-lr0.05-d0.25.yaml \
+# &python3 test_tfbind8_tune_23.py --config configs/tune_23/Template-BBDM-tfbind8-s17000-l5.8-lr0.05-d0.25.yaml \
+# &python3 test_tfbind8_tune_23.py --config configs/tune_23/Template-BBDM-tfbind8-s18000-l5.8-lr0.05-d0.25.yaml 
+
+# python3 train_for_all_1.py --config configs/tune_23/Template-BBDM-tfbind10-s10000-l5.8-lr0.05-d0.25.yaml --save_top 
 
 python3 test_tfbind10_tune_23.py --config configs/tune_23/Template-BBDM-tfbind10-l5.8-lr0.05-d0.25.yaml \
 &python3 test_tfbind10_tune_23_1.py --config configs/tune_23/Template-BBDM-tfbind10-l5.8-lr0.05-d0.25.yaml 
