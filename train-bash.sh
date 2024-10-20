dc5f8f3f6e9c9258b378f2315d59ceb2b21cde00
HungTT912 <noohung2003@gmail.com>
2024-10-20 10:26:18 +0700

    tune 23

--- train-bash.sh
+++ train-bash.sh
@@ -115,8 +115,8 @@
 
 # python3 train_for_all_1.py --config configs/tune_23/Template-BBDM-tfbind10-s10000-l5.25-lr0.05-d0.25.yaml --save_top 
 
-# python3 test_tfbind10_tune_23.py --config configs/tune_23/Template-BBDM-tfbind10-l5.25-lr0.05-d0.25.yaml \
-# &python3 test_tfbind10_tune_23_1.py --config configs/tune_23/Template-BBDM-tfbind10-l5.25-lr0.05-d0.25.yaml 
+python3 test_tfbind10_tune_23.py --config configs/tune_23/Template-BBDM-tfbind10-l5.25-lr0.05-d0.25.yaml \
+&python3 test_tfbind10_tune_23_1.py --config configs/tune_23/Template-BBDM-tfbind10-l5.25-lr0.05-d0.25.yaml 
 
 #----5.7
 # python3 train_for_all_1.py --config configs/tune_23/Template-BBDM-tfbind8-s10000-l5.7-lr0.05-d0.25.yaml --save_top \
