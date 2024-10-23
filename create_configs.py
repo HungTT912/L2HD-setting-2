aae8bb2f161c40c763dfbd03ab102fcdafa8ec0b
HungTT912 <noohung2003@gmail.com>
2024-10-23 10:45:27 +0700

    tune 23

--- create_configs.py
+++ create_configs.py
@@ -90,7 +90,7 @@ config_template = {
 }
 
 # Hyperparameter lists
-lengthscale = 5.8
+lengthscale = 5.3
 learning_rates = [0.05]
 delta_lengthscales = [0.25]
 num_fit_samples_list = [9000,10000,12000,13000,14000,15000,16000,17000,18000] 
