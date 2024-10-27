819a3e0cd88525dfdd99e3fab3daace79fa31284
HungTT912 <noohung2003@gmail.com>
2024-10-27 21:45:02 +0700

    tune 23

--- final_script.sh
+++ final_script.sh
@@ -1,5 +1,5 @@
 # python3 train_and_test_for_all.py --config configs/final/Template-L2HD-ant.yaml --save_top \
 # &python3 train_and_test_for_all.py --config configs/final/Template-L2HD-dkitty.yaml --save_top 
 
-python3 test_ant_tune_22_100steps.py --config configs\tune_20\Template-BBDM-ant-l1.0-lr0.001-d0.25.yaml --save_top \
-&python3 test_dkitty_tune_22_100steps.py --config configs\tune_20\Template-BBDM-dkitty-l1.0-lr0.001-d0.25.yaml --save_top 
+python3 test_ant_tune_22_100steps.py --config configs/tune_20/Template-BBDM-ant-l1.0-lr0.001-d0.25.yaml --save_top \
+&python3 test_dkitty_tune_22_100steps.py --config configs/tune_20/Template-BBDM-dkitty-l1.0-lr0.001-d0.25.yaml --save_top 
