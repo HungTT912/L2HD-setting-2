30ba07837b30f5809c0c1eb41e79d229d12e7771
HungTT912 <noohung2003@gmail.com>
2025-01-25 22:39:00 +0700

    tune 1

--- main.sh
+++ main.sh
@@ -1,6 +1,6 @@
-python3 test_setting_2.py --config configs/tune_1_setting_2/Template-L2HD-ant_10.yaml --save_top \
-&python3 test_setting_2.py --config configs/tune_1_setting_2/Template-L2HD-ant_20.yaml --save_top 
+python3 main.py --config configs/tune_1_setting_2/Template-L2HD-ant_10.yaml --save_top \
+&python3 main.py --config configs/tune_1_setting_2/Template-L2HD-ant_20.yaml --save_top 
 
-python3 test_setting_2.py --config configs/tune_1_setting_2/Template-L2HD-ant_50.yaml --save_top \
-&python3 test_setting_2.py --config configs/tune_1_setting_2/Template-L2HD-ant_100.yaml --save_top 
+python3 main.py --config configs/tune_1_setting_2/Template-L2HD-ant_50.yaml --save_top \
+&python3 main.py --config configs/tune_1_setting_2/Template-L2HD-ant_100.yaml --save_top 
 
